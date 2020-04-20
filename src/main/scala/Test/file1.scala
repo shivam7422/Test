03e29f9cd7f835515e5caa6cd627f35363e5622b
shivam7422 <63632045+shivam7422@users.noreply.github.com>
2020-04-20 22:34:59 +0530

    def details2 added

--- src/main/scala/Test/file1.scala
+++ src/main/scala/Test/file1.scala
@@ -7,10 +7,16 @@ class Admin
     val s= " Kadipur "
      println(" I live in " +s)
   }
+  def details2: Unit =
+  {
+    println(" this is something else")
+  }
+
 }
 object file1  extends App{
 
   val s= new Admin
   s.details
+  s.details2
 
 }
